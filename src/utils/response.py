12d34fc2de802aeb6418350c5c253db6be3146be
Deniz <denizergunn9@gmail.com>
2026-08-27 16:30:18 +0300

    Refactor response building functions in response.py

--- src/utils/response.py
+++ src/utils/response.py
@@ -1,15 +1,89 @@
-
 from sdks.novavision.src.helper.package import PackageHelper
-from components.Package.src.models.PackageModel import PackageModel, PackageConfigs, ConfigExecutor, PackageOutputs, PackageResponse, PackageExecutor, OutputImage
+from components.Parser.src.models.PackageModel import (
+    PackageModel,
+    PackageConfigs,
+    ConfigExecutor,
+    JsonParser,
+    JsonParserOutputs,
+    JsonParserResponse,
+    OutputData,
+    OutputErrorStatus,
+    VLMAsDetector,
+    VLMAsDetectorOutputs,
+    VLMAsDetectorResponse,
+    OutputDetections,
+    OutputInferenceId,
+    VLMAsClassifier,
+    VLMAsClassifierOutputs,
+    VLMAsClassifierResponse
+)
 
 
-def build_response(context):
-    outputImage = OutputImage(value=context.image)
-    Outputs = PackageOutputs(outputImage=outputImage)
-    packageResponse = PackageResponse(outputs=Outputs)
-    packageExecutor = PackageExecutor(value=packageResponse)
-    executor = ConfigExecutor(value=packageExecutor)
-    packageConfigs = PackageConfigs(executor=executor)
-    package = PackageHelper(packageModel=PackageModel, packageConfigs=packageConfigs)
-    packageModel = package.build_model(context)
-    return packageModel
+def build_json_parser_response(context):
+    output_data = OutputData(value=context.output_data)
+    output_error_status = OutputErrorStatus(value=context.error_status)
+
+    outputs = JsonParserOutputs(
+        outputData=output_data,
+        outputErrorStatus=output_error_status
+    )
+
+    response = JsonParserResponse(outputs=outputs)
+    executor = JsonParser(value=response)
+    config_executor = ConfigExecutor(value=executor)
+    package_configs = PackageConfigs(executor=config_executor)
+
+    package = PackageHelper(
+        packageModel=PackageModel,
+        packageConfigs=package_configs
+    )
+
+    return package.build_model(context)
+
+
+def build_vlm_as_detector_response(context):
+    output_detections = OutputDetections(value=context.output_detections)
+    output_error_status = OutputErrorStatus(value=context.error_status)
+    output_inference_id = OutputInferenceId(value=context.inference_id)
+
+    outputs = VLMAsDetectorOutputs(
+        outputDetections=output_detections,
+        outputErrorStatus=output_error_status,
+        outputInferenceId=output_inference_id
+    )
+
+    response = VLMAsDetectorResponse(outputs=outputs)
+    executor = VLMAsDetector(value=response)
+    config_executor = ConfigExecutor(value=executor)
+    package_configs = PackageConfigs(executor=config_executor)
+
+    package = PackageHelper(
+        packageModel=PackageModel,
+        packageConfigs=package_configs
+    )
+
+    return package.build_model(context)
+
+
+def build_vlm_as_classifier_response(context):
+    output_data = OutputData(value=context.output_data)
+    output_error_status = OutputErrorStatus(value=context.error_status)
+    output_inference_id = OutputInferenceId(value=context.inference_id)
+
+    outputs = VLMAsClassifierOutputs(
+        outputData=output_data,
+        outputErrorStatus=output_error_status,
+        outputInferenceId=output_inference_id
+    )
+
+    response = VLMAsClassifierResponse(outputs=outputs)
+    executor = VLMAsClassifier(value=response)
+    config_executor = ConfigExecutor(value=executor)
+    package_configs = PackageConfigs(executor=config_executor)
+
+    package = PackageHelper(
+        packageModel=PackageModel,
+        packageConfigs=package_configs
+    )
+
+    return package.build_model(context)
